bcf20d297739b642d5324e178c237cd32d4e268d
Mubyyy404 <mubeenmass577@gmail.com>
2025-12-09 20:03:49 +0530

    Enhance Log model with device and OS info
    Added new fields for device name and OS information in Log model.

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+# models.py
 import uuid
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
@@ -8,7 +9,6 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
-    # Auto-generate unique ID for the agent
     api_key = db.Column(db.String(36), unique=True, nullable=False, default=lambda: str(uuid.uuid4()))
     logs = db.relationship('Log', backref='owner', lazy=True)
 
@@ -17,4 +17,9 @@ class Log(db.Model):
     timestamp = db.Column(db.String(50))
     event_type = db.Column(db.String(50))
     filename = db.Column(db.String(200))
+    
+    # New Fields for V2.0
+    device_name = db.Column(db.String(100), default="Unknown")
+    os_info = db.Column(db.String(100), default="Unknown")
+    
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
